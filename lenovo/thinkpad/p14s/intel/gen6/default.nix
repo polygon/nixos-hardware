5276964cebfb629b115c11e61ac9c0529fceeacd
polygon <polygon@wh2.tu-dresden.de>
2025-11-03 23:47:43 +0100

    p14s gen6

--- lenovo/thinkpad/p14s/intel/gen6/default.nix
+++ lenovo/thinkpad/p14s/intel/gen6/default.nix
@@ -4,8 +4,11 @@
 }:
 {
   imports = [
-    ../../../common/cpu/intel/arrow-lake
+    ../../../../../common/cpu/intel/arrow-lake
     ../.
   ];
-};
+
+  #boot.kernelParams = [ "i915.force_probe=!7d51" "xe.force_probe=7d51" ];
+  #boot.blacklistedKernelModules = [ "intel_pmc_bxt" ];
+}
 
