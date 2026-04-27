756e073531f2ffe82a388ef9c1fa80e1599f2080
polygon <polygon@wh2.tu-dresden.de>
2026-04-27 20:12:21 +0200

    p14gen6: Enable NPU

--- lenovo/thinkpad/p14s/intel/gen6/default.nix
+++ lenovo/thinkpad/p14s/intel/gen6/default.nix
@@ -8,6 +8,8 @@
     ../.
   ];
 
+  hardware.cpu.intel.npu.enable = true;
+
   #boot.kernelParams = [ "i915.force_probe=!7d51" "xe.force_probe=7d51" ];
   #boot.blacklistedKernelModules = [ "intel_pmc_bxt" ];
 }
